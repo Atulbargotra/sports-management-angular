9c41c6efb9d2f04d8d19700bdd58ed496acac44e
atulverma <atul.bargotra@outlook.com>
2021-07-06 23:18:08 +0530

    merged and fixed all

--- src/app/layouts/userheader/userheader.component.ts
+++ src/app/layouts/userheader/userheader.component.ts
@@ -16,16 +16,14 @@ export class UserheaderComponent implements OnInit {
   notificationsAvailable: boolean = false;
   faBell = faBell;
   isLoggedIn: boolean;
-  showNoti:boolean=false;
-
-
+  showNoti: boolean = false;
+  notifications: NotificationResponsePayload[];
 
   constructor(
     private notificationService: NotificationService,
     private authService: AuthService,
     private router: Router
   ) {
-<<<<<<< HEAD
     this.notificationService.getUserNotificationsCount().subscribe(
       (count) => {
         this.count = count;
@@ -35,37 +33,9 @@ export class UserheaderComponent implements OnInit {
         this.notificationsAvailable = true;
       }
     );
-=======
-    // this.loadNotifications();
->>>>>>> 04ad74a28154ae9a5a3404e3b611f5bda5d421b8
   }
 
   ngOnInit(): void {}
-
-
-  notifications: NotificationResponsePayload[]=[
-    {
-      eventName:'Cricket',
-      message:'Cricket Winners Announced',
-      picture:'https://firebasestorage.googleapis.com/v0/b/login-authentication-45ce5.appspot.com/o/cricket.jpeg?alt=media&token=142af2aa-52c5-49ef-93e0-964bb3651e8d'
-    },
-    {
-      eventName:'Basketball',
-      message:'Basketball Winners Announced',
-      picture:'https://firebasestorage.googleapis.com/v0/b/login-authentication-45ce5.appspot.com/o/basketball.jpeg?alt=media&token=d1bad8a4-9246-4900-b00e-2fc1823b94d7'
-    },
-    {
-      eventName:'Chess',
-      message:'Chess Winners Announced',
-      picture:'https://firebasestorage.googleapis.com/v0/b/login-authentication-45ce5.appspot.com/o/chess.jpeg?alt=media&token=506d50ee-fb2e-48cc-aac4-433394248fd0'
-    },
-    {
-      eventName:'Volleyball',
-      message:'Volleyball Winners Announced',
-      picture:'https://firebasestorage.googleapis.com/v0/b/login-authentication-45ce5.appspot.com/o/vollyball.jpeg?alt=media&token=43409a2d-464a-4981-a36a-d755575ad104'
-    },
-  ]
-
 
   //Signout feature
   logout() {
@@ -73,27 +43,18 @@ export class UserheaderComponent implements OnInit {
     this.isLoggedIn = false;
     this.router.navigateByUrl('');
   }
-<<<<<<< HEAD
   loadNotifications() {
+    this.showNoti = !this.showNoti;
     this.notificationService.getUserNotifications().subscribe(
       (data) => {
-        this.notificationsAvailable = true;
+        console.log(data);
         this.notifications = data;
-        this.count = this.notifications.length;
-        console.log(this.count);
       },
       () => {
-        this.notificationsAvailable = false;
+        this.showNoti = false;
       }
     );
-=======
-
-  //trigger notification on click
-  triggerNotificationBar(){
-    this.showNoti = !this.showNoti;
->>>>>>> 04ad74a28154ae9a5a3404e3b611f5bda5d421b8
   }
-
 
   //fetching notification
   // loadNotifications() {
@@ -109,8 +70,4 @@ export class UserheaderComponent implements OnInit {
   //     }
   //   );
   // }
-
-  
-
-
 }
