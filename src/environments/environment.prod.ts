070477fa58fce3a15309f4d78c9973b685000a43
Atul Verma <44652100+Atulbargotra@users.noreply.github.com>
2021-07-25 18:57:53 +0530

    Update environment.prod.ts

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,12 +1,12 @@
 export const environment = {
   production: true,
   firebaseConfig: {
-    apiKey: 'AIzaSyCvOKIbPKyDBBKlSqanqmsQuGDQqSa5iKc',
-    authDomain: 'sportseventmanagement-a3d28.firebaseapp.com',
-    projectId: 'sportseventmanagement-a3d28',
-    storageBucket: 'sportseventmanagement-a3d28.appspot.com',
-    messagingSenderId: '123258266750',
-    appId: '1:123258266750:web:335a1cc24c58855b22a42f',
+    apiKey: '',
+    authDomain: '',
+    projectId: '',
+    storageBucket: '',
+    messagingSenderId: '',
+    appId: '',
   },
   baseUrl: 'https://tiaasports.herokuapp.com/',
 };
